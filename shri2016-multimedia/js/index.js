29fd33b4c1a974610ddfea6157ac6d1a8446f69b
maksimn <maksim1989sovunion@yandex.ru>
2016-07-17 21:50:56 +0300

    Multimedia URL data input implemented

--- shri2016-multimedia/js/index.js
+++ shri2016-multimedia/js/index.js
@@ -1,1 +1,15 @@
-﻿
+﻿window.onload = function () {
+    var url = new URL(window.location.href);
+    var searchParams = url.searchParams;
+
+    if(searchParams.toString() === "") {
+        return
+    }
+
+    var videoUrl = searchParams.get('video');
+    var subsUrl = searchParams.get('subs');
+    var audioUrl = searchParams.get('audio');
+
+    var ans = "Video URL: " + videoUrl + "\n\nSubtitles URL: " + subsUrl + "\n\nAudio URL: " + audioUrl;
+    alert(ans);
+}
