a52c630d7da9089422bfa59e98429aa6fc05432b
maksimn <maksim1989sovunion@yandex.ru>
2016-07-18 04:30:40 +0300

    Ready to implement processing

--- shri2016-multimedia/js/index.js
+++ shri2016-multimedia/js/index.js
@@ -2,14 +2,41 @@
     var url = new URL(window.location.href);
     var searchParams = url.searchParams;
 
-    if(searchParams.toString() === "") {
-        return
+    if (searchParams.toString() === "") {
+        show(document.querySelector('.silencer_start'));
+        hide(document.querySelector('.silencer_work'));
+        return;
     }
+
+    hide(document.querySelector('.silencer_start'));
+    show(document.querySelector('.silencer_work'));
+    document.getElementById('return_back_link').href = url.origin + url.pathname;
 
     var videoUrl = searchParams.get('video');
     var subsUrl = searchParams.get('subs');
     var audioUrl = searchParams.get('audio');
 
-    var ans = "Video URL: " + videoUrl + "\n\nSubtitles URL: " + subsUrl + "\n\nAudio URL: " + audioUrl;
-    alert(ans);
+    var auEl = document.querySelector('audio');
+    auEl.src = audioUrl;
+    auEl.play();
+
+    var videoEl = document.querySelector('video');
+    videoEl.src = videoUrl;
+    videoEl.play();
+
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var fileText = xmlhttp.responseText;
+        }
+    };
+    xmlhttp.open("GET", subsUrl, true);
+    xmlhttp.send();
+
+    function hide(el) {
+        el.style.display = 'none';
+    }
+    function show(el) {
+        el.style.display = 'block';
+    }
 }
