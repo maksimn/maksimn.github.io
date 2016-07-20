149a1d34b63f9e7448e8d4a6118e30f31bbbc6e6
maksimn <maksim1989sovunion@yandex.ru>
2016-07-20 19:43:23 +0300

    First fugly version. Last thing to do -- add Play&Stop button

--- shri2016-multimedia/js/index.js
+++ shri2016-multimedia/js/index.js
@@ -48,6 +48,7 @@
         xmlhttp.onreadystatechange = function () {
             if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
                 var fileText = xmlhttp.responseText;
+                initSrt(fileText);
                 callback();
             }
         };
@@ -68,26 +69,100 @@
     }
     function playVideoAndDrawOnCanvas(ctx) {
         var i, videoEl = document.getElementById('video');
+        var subNum = 0; // номер следующего субтитра, который нужно показать
+        var videoCurrentTime = document.getElementById('videoCurrentTime');
+        var subtitlesStart = document.getElementById('subtitlesStart');
+        var subtitleText = document.getElementById('subtitleText');
+
+        var sub = document.getElementById('subArea');
 
         videoEl.addEventListener('play', _onPlayDrawVideoFramesOnCanvas, false);
         videoEl.addEventListener('pause', _onPauseStopDrawVideoFramesOnCanvas, false);
-        videoEl.addEventListener('ended', _onPauseStopDrawVideoFramesOnCanvas, false);
+        videoEl.addEventListener('ended', _onVideoEnded, false);
+
+        function _onVideoEnded(e) {
+            videoEl.currentTime = 0;
+            videoEl.pause();
+            document.querySelector('audio').pause();
+        }
 
         function _onPlayDrawVideoFramesOnCanvas(e) {
             i = window.setInterval(function () {
-                ctx.drawImage(videoEl, 0, 0, VIDEO_WIDTH, VIDEO_HEIGHT);
-                var imgData = ctx.getImageData(0, 0, VIDEO_WIDTH, VIDEO_HEIGHT);
-                for (var j = 0; j < imgData.data.length; j += 4) {
-                    var grey = greyFromRGB(imgData.data[j], imgData.data[j + 1], imgData.data[j + 2]);
-                    imgData.data[j + 2] = imgData.data[j + 1] = imgData.data[j] = grey;
+                videoCurrentTime.innerText = videoEl.currentTime;
+                subtitlesStart.innerText = srt[subNum].start;
+
+                /* Здесь нужно написать код, обрабатывающий показ субтитров */                
+                if (videoEl.currentTime >= srt[subNum].start) {
+                    videoEl.pause();
+                } else {
+                    /* Конец кода, обрабатывающего показ субтитров */
+                    ctx.drawImage(videoEl, 0, 0, VIDEO_WIDTH, VIDEO_HEIGHT);
+                    var imgData = ctx.getImageData(0, 0, VIDEO_WIDTH, VIDEO_HEIGHT);
+                    for (var j = 0; j < imgData.data.length; j += 4) {
+                        var grey = greyFromRGB(imgData.data[j], imgData.data[j + 1], imgData.data[j + 2]);
+                        imgData.data[j + 2] = imgData.data[j + 1] = imgData.data[j] = grey;
+                    }
+                    ctx.putImageData(imgData, 0, 0);
                 }
-                ctx.putImageData(imgData, 0, 0);
             }, INTERVAL_BETWEEN_FRAMES);
             document.getElementById('scratches_video').play();
         }
         function _onPauseStopDrawVideoFramesOnCanvas(e) {
             window.clearInterval(i);
+            ctx.fillRect(0, 0, VIDEO_WIDTH, VIDEO_HEIGHT);
+            subtitleText.innerText = srt[subNum].text;
+            sub.innerText = srt[subNum].text;
+            setTimeout(function () {
+                videoEl.play();
+                subtitleText.innerText = "";
+                sub.innerText = "";
+            }, srt[subNum++].duration * 1000);
         }
+    }
+    function initSrt(srtFileText) {
+        var num = 1, pos = 0;
+        var dt = 0.0, k = 1;
+        var ch;
+        function readChunk() {
+            var ind = srtFileText.indexOf('\n' + (num + 1) + '\n', pos);
+            if (ind === -1) {
+                return "";
+            }
+            var res = srtFileText.substring(pos, ind);
+            pos = ind + 1;
+            num++;
+            return res;
+        }
+        function readLastChunk() {
+            var ind = srtFileText.lastIndexOf('\n' + num + '\n');
+            return srtFileText.substring(ind + 1);
+        }
+        function getObjForSrt() {
+            var ind1 = ch.indexOf('\n');
+            var ind2 = ch.indexOf('\n', ind1 + 1);
+            var timeStr = ch.substring(ind1 + 1, ind2);
+            var hh = parseInt(timeStr.substring(0, 2));
+            var mm = parseInt(timeStr.substring(3, 5));
+            var ss = parseInt(timeStr.substring(6, 8));
+            var ms = parseInt(timeStr.substring(9, 12));
+            var tBegin = hh * 3600 + mm * 60 + ss + ms * 0.001;
+            hh = parseInt(timeStr.substring(17, 19));
+            mm = parseInt(timeStr.substring(20, 22));
+            ss = parseInt(timeStr.substring(23, 25));
+            ms = parseInt(timeStr.substring(26, 29));
+            var tEnd = hh * 3600 + mm * 60 + ss + ms * 0.001;
+            var txt = ch.substring(ind2 + 1);
+            return {
+                start: tEnd + dt,
+                duration: k * (tEnd - tBegin),
+                text: txt
+            };
+        }
+        while((ch = readChunk()).length > 0) {
+            srt.push(getObjForSrt());
+        }
+        ch = readLastChunk();        
+        srt.push(getObjForSrt());
     }
 }
 
